d01c36ddedcd2ee1c7ca428fddcdcb818ccdb63e
Suryadeep Pal <suryadeep10@gmail.com>
2021-12-06 12:33:48 +0530

    feat: initial commit with github action to update notion tasks

---
+++ update_notion/index.js
@@ -0,0 +1,93 @@
+const core = require('@actions/core')
+const { Octokit } = require('@octokit/rest')
+
+const Notion = require('./../utils/notion')
+
+try {
+  const {
+    GITHUB_REF,
+  } = process.env
+
+  if (GITHUB_REF === 'refs/heads/master') {
+    _updateNotionStatuses('master')
+  } else if (GITHUB_REF === 'refs/heads/staging') {
+    _updateNotionStatuses('staging')
+  } else if (GITHUB_REF === 'refs/heads/dev') {
+    _updateNotionStatuses('dev')
+  }
+} catch (error) {
+  core.setFailed(error.message)
+}
+
+/**
+ * Function to update the status of the tasks based on
+ * the recent commit is getting merged to which branch
+ *
+ * @param {'dev' | 'staging' | 'master' } branch Name of the branch to which the commit is getting merged to
+ */
+async function _updateNotionStatuses (branch) {
+
+  const {
+    GITHUB_REPOSITORY,
+    GITHUB_TOKEN,
+    GITHUB_OWNER,
+  } = process.env
+
+  const octokit = new Octokit({
+    auth: GITHUB_TOKEN,
+  })
+
+  const NOTION_DATABASE_TOKEN = core.getInput('NOTION_DATABASE_TOKEN')
+  const NOTION_API_KEY = core.getInput('NOTION_API_KEY')
+
+  const notionUtil = new Notion({
+    apiKey: NOTION_API_KEY,
+    databaseToken: NOTION_DATABASE_TOKEN,
+  })
+
+  const repositoryName = GITHUB_REPOSITORY.split('/').pop()
+  
+  // Get most recent commit to branch
+  const { data } = await octokit.rest.repos.getCommit({
+    owner: GITHUB_OWNER,
+    repo: GITHUB_REPOSITORY,
+    ref: branch,
+    perPage: 1,
+    page: 1,
+  })
+
+  const {
+    commit: {
+      message: commitMessage,
+    },
+  } = data
+
+  switch (branch) {
+    case 'master':
+      if (commitMessage.includes('from coursedog/staging')) {
+        // Update all tasks that are in Completed Staging to Completed Prod
+        notionUtil.updateByStatus('Completed (Staging)', 'Completed (Production)')
+      }
+      break
+    case 'staging':
+      if (commitMessage.includes('from coursedog/dev')) {
+        // Update all tasks that are in Completed Dev to Completed Staging
+        notionUtil.updateByStatus('Completed (Dev)', 'Completed (Staging)')
+      } else if (commitMessage.match(/#+[0-9]/)) {
+        // direct from open PR to staging
+        const prNumber = commitMessage.split('#')[1].replace(/\D/g, '')
+        notionUtil.updateByPR(`${repositoryName}/pull/${prNumber}`, 'Completed (Staging)')
+      }
+      break
+    case 'dev':
+      if (commitMessage.match(/#+[0-9]/)) {
+        // direct from open PR to dev
+        const prNumber = commitMessage.split('#')[1].replace(/\D/g, '')
+        notionUtil.updateByPR(`${repositoryName}/pull/${prNumber}`, 'Completed (Dev)')
+      }
+      break
+  
+    default:
+      break
+  }
+}
---
+++ utils/notion.js
@@ -0,0 +1,121 @@
+const { Client }= require('@notionhq/client')
+
+class Notion {
+  constructor ({ apiKey, databaseToken }) {
+    // Retrieving all the input parameters relevant to notion
+    this.notionDatabaseToken = databaseToken
+      
+    // Initializing the notion client
+    this.client = new Client({ auth: apiKey })
+    this.labels = {
+      GITHUB_PR: 'GitHub PR',
+      STATUS: 'Status',
+      DATE_COMPLETED: 'Date Completed',
+    }
+  }
+
+  /**
+   * Function to update the status of a particular task from one to the other one
+   *
+   * @param {String} originalStatus Status from which the task is to be moved
+   * @param {String} newStatus Status to which the status is to be moved
+   */
+  async updateByStatus (originalStatus, newStatus) {
+    console.log('Updating all Notion tasks from', originalStatus, 'to', newStatus)
+    const databaseId = this.notionDatabaseToken
+    const {
+      GITHUB_REPOSITORY,
+    } = process.env
+    const repositoryName = GITHUB_REPOSITORY.split('/').pop()
+
+    const listOfTasks = await this.client.databases.query({
+      database_id: databaseId,
+      filter: {
+        and: [
+          {
+            property: this.labels.STATUS,
+            select: {
+              equals: originalStatus,
+            },
+          },
+          {
+            property: this.labels.GITHUB_PR,
+            text: {
+              contains: repositoryName,
+            },
+          },
+        ],
+      },
+    })
+    for (let i = 0; i < listOfTasks.results.length; i++) {
+      const updatedTasks = await this.client.pages.update({
+        page_id: listOfTasks.results[i].id,
+        properties: {
+          Status: {
+            select: {
+              name: newStatus,
+            },
+          },
+        },
+      })
+      console.log('Updated', updatedTasks)
+    }
+  }
+
+  /**
+   * Function to promote the task by the Github PR field
+   *
+   * @param {String} pr PR URL for which the status is to be updated
+   * @param {String} newStatus New status to which the task is to be promoted
+   */
+  async updateByPR (pr, newStatus) {
+    console.log('Updating one Notion task with PR', pr, 'to', newStatus)
+    const databaseId = this.notionDatabaseToken
+    const listOfTasks = await this.client.databases.query({
+      database_id: databaseId,
+      filter: {
+        and: [
+          {
+            property: this.labels.GITHUB_PR,
+            text: {
+              ends_with: pr,
+            },
+          },
+          {
+            property: this.labels.STATUS,
+            select: {
+              does_not_equal: 'Completed (old)',
+            },
+          },
+          {
+            property: this.labels.STATUS,
+            select: {
+              does_not_equal: 'Completed (Production)',
+            },
+          },
+        ],
+      },
+    })
+    for (let i = 0; i < listOfTasks.results.length; i++) {
+      const updatedTasks = await this.client.pages.update({
+        page_id: listOfTasks.results[i].id,
+        properties: {
+          [this.labels.STATUS]: {
+            select: {
+              name: newStatus,
+            },
+          },
+          [this.labels.DATE_COMPLETED]: {
+            date: {
+              start: new Date().toISOString(),
+            },
+          },
+        },
+      })
+      console.log('Updated', updatedTasks)
+    }
+  }
+  
+}
+
+module.exports = Notion
